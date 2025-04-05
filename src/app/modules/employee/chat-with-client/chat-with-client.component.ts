6ef5ef72680fdf06f103b4ff931a65c4997bcd7c
vishnupriyaab <vishnupriyaotp2002@gmail.com>
2025-04-06 01:05:33 +0530

    user and employee side chat UI is done

--- src/app/modules/employee/chat-with-client/chat-with-client.component.ts
+++ src/app/modules/employee/chat-with-client/chat-with-client.component.ts
@@ -1,9 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat-with-client',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './chat-with-client.component.html',
   styleUrl: './chat-with-client.component.css',
 })
-export class ChatWithClientComponent {}
+export class ChatWithClientComponent {
+
+  message: string = '';
+
+  sendMessage() {
+    if (this.message.trim()) {
+      console.log('Sending:', this.message);
+      // Implement send logic here...
+      this.message = ''; // Clear input after sending
+    }
+  }
+}
--- src/app/modules/user/chat-with-employee/chat-with-employee.component.ts
+++ src/app/modules/user/chat-with-employee/chat-with-employee.component.ts
@@ -1,9 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat-with-employee',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './chat-with-employee.component.html',
   styleUrl: './chat-with-employee.component.css',
 })
-export class ChatWithEmployeeComponent {}
+export class ChatWithEmployeeComponent {
+
+  message: string = '';
+
+  sendMessage() {
+    if (this.message.trim()) {
+      console.log('Sending:', this.message);
+      // Implement send logic here...
+      this.message = ''; // Clear input after sending
+    }
+  }
+}
